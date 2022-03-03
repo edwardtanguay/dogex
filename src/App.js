3ca80fc6af493fb0a1bcd912ae4667d7d2d91460
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-03 16:53:39 +0100

    gets data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 
+const url = 'https://api.thedogapi.com/v1/images/search?limit=5';
+
 function App() {
-  return (
-    <div className="App">
-		<p>Hello</p>
-    </div>
-  );
+
+	useEffect(() => {
+		(async () => {
+			const response = await fetch(url);
+			const data = await response.json();
+			console.log(data);
+		})();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Dog Breeds</h1>
+		</div>
+	);
 }
 
 export default App;
